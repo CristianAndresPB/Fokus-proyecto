7088d6ff41accbc972957b64b562f7ae74578f17
Cristian Perez <cristian.perezb1991@gmail.com>
2024-11-01 23:20:25 -0500

    Aplicando audio y estilos

--- script.js
+++ script.js
@@ -4,26 +4,50 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque')
 const botonLargo = document.querySelector('.app__card-button--largo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const botones = document.querySelectorAll('.app__card-button')
+const inputEnfoqueMusica = document.querySelector('#alternar-musica')
+const musica = new Audio('./sonidos/luna-rise-part-one.mp3')
+
+musica.loop = true // sirve para escuchar la musica por el tiempo que uno desee
+
+inputEnfoqueMusica.addEventListener('change', ()=> {
+    if(musica.paused){
+        musica.play()
+        
+    }else{
+        musica.pause()
+    }
+
+})
 
 botonCorto.addEventListener('click', () => {
     cambiarContexto ('descanso-corto') 
+    botonCorto.classList.add('active')
     //html.setAttribute('data-contexto', 'descanso-corto')
     //banner.setAttribute('src', './imagenes/descanso-corto.png')
 })
 
 botonEnfoque.addEventListener('click', () => {
     cambiarContexto ('enfoque')
+    botonEnfoque.classList.add('active')
     //html.setAttribute('data-contexto', 'enfoque' )
     //banner.setAttribute('src', './imagenes/enfoque.png')
 })
 
 botonLargo.addEventListener('click', () => {
     cambiarContexto ('descanso-largo')
+    botonLargo.classList.add('active')
     //html.setAttribute('data-contexto', 'descanso-largo' )
     //banner.setAttribute('src', './imagenes/descanso-largo.png')
 })
 
 function cambiarContexto(contexto){
+
+    botones.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
+
+
     html.setAttribute('data-contexto',contexto)
     banner.setAttribute('src', `./imagenes/${contexto}.png`)
 
