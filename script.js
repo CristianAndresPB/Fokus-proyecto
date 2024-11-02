ef93d2277019aba7232a6527f0a8f4068da63e2b
Cristian Perez <cristian.perezb1991@gmail.com>
2024-11-01 22:34:41 -0500

    manipulando elementos de la pagina

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
 const html = document.querySelector('html')
 const botonCorto = document.querySelector('.app__card-button--corto')
 const botonEnfoque = document.querySelector('.app__card-button--enfoque')
+const botonLargo = document.querySelector('.app__card-button--largo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 botonCorto.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-corto')
+    cambiarContexto ('descanso-corto') 
+    //html.setAttribute('data-contexto', 'descanso-corto')
+    //banner.setAttribute('src', './imagenes/descanso-corto.png')
 })
 
 botonEnfoque.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'enfoque' )
+    cambiarContexto ('enfoque')
+    //html.setAttribute('data-contexto', 'enfoque' )
+    //banner.setAttribute('src', './imagenes/enfoque.png')
 })
+
+botonLargo.addEventListener('click', () => {
+    cambiarContexto ('descanso-largo')
+    //html.setAttribute('data-contexto', 'descanso-largo' )
+    //banner.setAttribute('src', './imagenes/descanso-largo.png')
+})
+
+function cambiarContexto(contexto){
+    html.setAttribute('data-contexto',contexto)
+    banner.setAttribute('src', `./imagenes/${contexto}.png`)
+
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML = `
+            Optimiza tu productividad,<br>
+                <strong class="app__title-strong">sumérgete en lo que importa.</strong>`
+            
+            break;
+        case "descanso-corto":
+            titulo.innerHTML = `
+            ¿Qué tal tomar un respiro? 
+           <strong class="app__title-strong"> ¡Haz una pausa corta!</strong>
+           `
+           break;
+           case "descanso-largo":
+            titulo.innerHTML = `
+            Hora de volver a la superficie
+            <strong class="app__title-strong"> Haz una pausa larga.</strong>
+            `
+
+        default:
+            break;
+    }
+
+
+}
